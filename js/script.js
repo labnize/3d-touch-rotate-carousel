6eefb9c22c2b24c5893897944014c63417b88513
labnize <zhulixh@126.com>
2017-11-06 15:46:49 +0800

    定时自动旋转

--- js/script.js
+++ js/script.js
@@ -68,20 +68,26 @@ $(document).ready(function () {
 
   $('#phoneCarousel .previous').click(function () {
     // 360/cnt lets us distribute the phones evenly in a circle
+    clearInterval(timer);
+    timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
+
     rotate(-10, 360 / cnt);
   });
 
   $('#phoneCarousel .next').click(function () {
+    clearInterval(timer);
+    timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
+
     rotate(10, 360 / cnt);
   });
-  $('#phoneCarousel .previous,#phoneCarousel .next').hover(function () {
-    clearInterval(timer);
-  }, function () {
-    // timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
-  });
+  // $('#phoneCarousel .previous,#phoneCarousel .next').hover(function () {
+  //   clearInterval(timer);
+  // }, function () {
+  //   timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
+  // });
   var timer = null;
 
-  // timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
+  timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
 
 
   function contentCtroller() {
@@ -94,6 +100,8 @@ $(document).ready(function () {
       startX = touche.clientX;
     });
     contentBox.addEventListener("touchend", function (event) {
+      clearInterval(timer);
+      timer=setInterval(function(){rotate(10,360/cnt);},4000);  //定时旋转
       var touche = event.changedTouches[0];
       disX = touche.clientX - startX;
       if (disX > 0 && disX > 100) {
